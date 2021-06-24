12df59f54a97394ba872305ed69a24f280082b6d
bryanatg <bryanatg@hotmail.com>
2021-06-23 19:22:44 -0500

    Deber Implementaciones

--- Modulo1/Basic/10_Clases.js
+++ Modulo1/Basic/10_Clases.js
@@ -36,34 +36,34 @@
 */
 //  1.- Implementar getters , setters y realizar una implementacion.
 
-// class Persona {
-//     constructor(nombre,apellido,edad,direccion){
-//         this.nombre = nombre
-//         this.apellido = apellido
-//         this.edad = edad
-//         this.direccion = direccion
-//     }
+    class Persona {
+        constructor(nombre,apellido,edad,direccion){
+            this.nombre = nombre
+            this.apellido = apellido
+            this.edad = edad
+            this.direccion = direccion
+        }
 
-//     get Datos_Persona(){
-//         return {
-//             nombre: this.nombre,
-//             apellido: this.apellido,
-//             edad: this.edad,
-//             direccion: this.direccion
-            
-//         }
-//     }
-//     set cambio_Datos(nuevos){
-//         this.nombre = nuevos.nombre
-//         this.apellido = nuevos.apellido
-//         this.edad = nuevos.edad
-//         this.direccion = nuevos.direccion
-//     }
-// }
-// var persona = new Persona(`Bryan`,`Toapanta`,`25`,`Carcelen`)
-// console.log(persona.Datos_Persona);
-// persona.cambio_Datos = {nombre:`Andres`,apellido:`Granja`,edad:`21`,direccion:`El Condado`}
-// console.log(persona.Datos_Persona);
+        get Datos_Persona(){
+            return {
+                nombre: this.nombre,
+                apellido: this.apellido,
+                edad: this.edad,
+                direccion: this.direccion
+                
+            }
+        }
+        set cambio_Datos(nuevos){
+            this.nombre = nuevos.nombre
+            this.apellido = nuevos.apellido
+            this.edad = nuevos.edad
+            this.direccion = nuevos.direccion
+        }
+    }
+    var persona = new Persona(`Bryan`,`Toapanta`,`25`,`Carcelen`)
+    console.log(persona.Datos_Persona);
+    persona.cambio_Datos = {nombre:`Andres`,apellido:`Granja`,edad:`21`,direccion:`El Condado`}
+    console.log(persona.Datos_Persona);
 /*      Salida en Consola
 {   
     nombre: 'Bryan',
@@ -80,23 +80,23 @@
 
     //Implementacion metodos estaticos - Clase Punto, Function distancia
   
-//   class Punto {
-//     constructor(titulo, fecha) {
-//       this.titulo = titulo;
-//       this.fecha = fecha;
-//     }
-  
-//     static Distancia(articuloA, articuloB) {
-//       return articuloA.fecha - articuloB.fecha;
-//     }
-//   }
-//   let articulos = [
-//     new Punto("HTML", new Date(2019, 1, 1)),
-//     new Punto("CSS", new Date(2019, 0, 1)),
-//     new Punto("JavaScript", new Date(2019, 11, 1))
-//   ];
-//   articulos.sort(Punto.Distancia);
-//   console.log( articulos[0].titulo );
+    class Punto {
+        constructor(titulo, fecha) {
+        this.titulo = titulo;
+        this.fecha = fecha;
+        }
+    
+        static Distancia(articuloA, articuloB) {
+        return articuloA.fecha - articuloB.fecha;
+        }
+    }
+    let articulos = [
+        new Punto("HTML", new Date(2019, 1, 1)),
+        new Punto("CSS", new Date(2019, 0, 1)),
+        new Punto("JavaScript", new Date(2019, 11, 1))
+    ];
+    articulos.sort(Punto.Distancia);
+    console.log( articulos[0].titulo );
 
   //Construir Clase Persona y estudiante, extender Estudiante con Persona
     

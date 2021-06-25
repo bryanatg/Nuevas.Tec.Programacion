3a5eb26e41390e335d2c4bb57ae32ed65435bdfd
bryanatg <bryanatg@hotmail.com>
2021-06-24 19:13:49 -0500

    Servicios Web

--- Modulo1/Programming/01_Projects/index.js
+++ Modulo1/Programming/01_Projects/index.js
@@ -8,6 +8,29 @@ app.get('/', (req, res) => {
     res.send("Hola Mundo con ExpressJS")
 })
 
+app.get("/saludo", (req, res) => {
+    //req.query
+    // console.log(req.query);
+    const { query: {nombre, apellido}
+    } = req;
+
+    // const nombre = req.query.nombre
+    // const apellido = req.query.apellido
+
+    res.json({
+        saludo: `Hola soy  ${nombre} ${apellido}`,
+    });
+});
+
+app.get('/saludo/:nombre', (req, res) => {
+    const { params: { nombre }, 
+    } = req;
+
+    res.json({
+        nombre: nombre,
+    })
+});
+
 app.listen(3000, () => {
     console.log("Servidor Escuchado en  http://localhost:3000")
 })
---
+++ Modulo1/Programming/01_Projects/routes/index.js
@@ -0,0 +1,10 @@
+//Traemos el paquete 
+const { Router } = require('express');
+
+//Inicializamos el Router
+const router = Router()
+
+//CReamos el endPoint
+
+//Exportamos las Rutas
+module.exports.RouterIndex = router

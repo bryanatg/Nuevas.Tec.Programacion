465100935da9c6638c866e3c53dd76708672820c
bryanatg <bryanatg@hotmail.com>
2021-06-25 12:07:53 -0500

    Trabajo_Clase

--- Modulo1/Programming/01_Projects/index.js
+++ Modulo1/Programming/01_Projects/index.js
@@ -2,35 +2,16 @@
 const express = require('express'); 
 
 //Genero de una Aplicacion
-const app  = express()
-//EndPoint
-app.get('/', (req, res) => {
-    res.send("Hola Mundo con ExpressJS")
-})
+const app  = express();
 
-app.get("/saludo", (req, res) => {
-    //req.query
-    // console.log(req.query);
-    const { query: {nombre, apellido}
-    } = req;
-
-    // const nombre = req.query.nombre
-    // const apellido = req.query.apellido
-
-    res.json({
-        saludo: `Hola soy  ${nombre} ${apellido}`,
-    });
-});
-
-app.get('/saludo/:nombre', (req, res) => {
-    const { params: { nombre }, 
-    } = req;
-
-    res.json({
-        nombre: nombre,
-    })
-});
+//Routers
+const {RouterIndex} = require('./routes/index');
+app.use( RouterIndex)
+// app.use("/users", RouterIndex)
+// app.use("/users/data", RouterIndex)
+app.use(RouterIndex)
 
 app.listen(3000, () => {
     console.log("Servidor Escuchado en  http://localhost:3000")
 })
+
--- Modulo1/Programming/01_Projects/routes/index.js
+++ Modulo1/Programming/01_Projects/routes/index.js
@@ -5,6 +5,28 @@ const { Router } = require('express');
 const router = Router()
 
 //CReamos el endPoint
+router
+    .get("/", (req, res) => {
+        res.send("Hola Mundo con ExpressJS")
+    })
+
+  
+
+    .get("/saludo", (req, res) => {
+        const { query } = req;
+        res.json({
+            saludo: `Hola soy  ${query.nombre} ${query.apellido}`,
+        });
+
+    })
+    .get("/saludo/:nombre", (req, res) => {
+        const { params } = req;
+    
+        res.json({
+            nombre: params.nombre,
+        })
+    });
+    
 
 //Exportamos las Rutas
 module.exports.RouterIndex = router

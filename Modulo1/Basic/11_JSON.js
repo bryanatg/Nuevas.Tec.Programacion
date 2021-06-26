0e38712130b8f5d8f0d1850d028cae88e8993469
bryanatg <bryanatg@hotmail.com>
2021-06-25 21:00:07 -0500

    update

--- Modulo1/Basic/11_JSON.js
+++ Modulo1/Basic/11_JSON.js
@@ -5,9 +5,13 @@ const data = require('./data.json');
 var modifiedData = data.map((person) => {
 
     person.first_name = person.first_name.toLocaleUpperCase()
+    person.last_name = person.last_name.toLocaleUpperCase()
+
     return person
 })
+
 // console.log(modifiedData)
 
-var filterResult = modifiedData.filter((person) => person.id === 3)
+var filterResult = modifiedData.filter((person) => person.id === person.id)
+
 console.log(filterResult)
